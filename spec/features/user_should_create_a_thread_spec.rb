b1fbc88cdea31b3ef204d02d1bc9078fe06cf1dc
William Holt <holtw@musc.edu>
2016-05-04 18:54:23 -0400

    changed controller name to topic

---
+++ spec/features/user_should_create_a_thread_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User should create a thread' do
+  scenario 'successfully' do
+    user = create(:user)
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in 'user_password', with: user.password
+    click_button 'Log in'
+
+    visit root_path
+
+    click_link 'Create a Thread'
+
+    fill_in 'thread_name', with: 'ooga booga'
+    click_button 'Create Thread'
+
+    expect(page).to have_css 'a', text: 'ooga booga'
+  end
+end
