7083031b422aeb8788b5178a38fda02794d602ca
William Holt <holtw@musc.edu>
2016-05-05 20:00:24 -0400

    added from in mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -2,6 +2,7 @@ class WelcomeMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: 'Thanks for your participation')
+    mail(from: 'thissite@donotreply.com', to: @user.email,
+         subject: 'Thanks for your participation')
   end
 end
